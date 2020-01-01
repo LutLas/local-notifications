e8e0665bb7714a432e1577c750384f31404f1de5
Muyoba Lutlas <lutakachihota@gmail.com>
2020-01-01 17:48:33 +0200

    3rd commit
    wordpress intergrated

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,6 +13,101 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  constructor() {}
+	url: string = 'http://localhost:81/wordpress/'
+  items: any = []
+  page: any = 1;
 
+  constructor(
+
+  	 public http: HttpClient,
+    public Router: Router,
+    public toastCtrl: ToastController,
+    public loadingController: LoadingController
+
+  	) {
+    this.loadPost(this.url, this.page, true);
+  }
+  async loadPost(url: string, page, showLoading) {
+    const loading = await this.loadingController.create({
+      message: 'Loading Your posts'
+    });
+    if (showLoading) {
+      await loading.present();
+    }
+
+    const route = this.url + 'wp-json/wp/v2/posts'
+    // set pagination
+    if (!page) {
+      page = '1';
+    }
+
+    return new Promise((resolve, reject) => {
+
+      var concat;
+
+      // check if url already has a query param
+      if (url.indexOf('?') > 0) {
+        concat = '&';
+      } else {
+        concat = '?';
+      }
+
+      this.http.get(route + concat + 'page=' + page)
+        .subscribe(data => {
+
+          if (showLoading) {
+            loading.dismiss();
+          }
+          this.items = data;
+          resolve(this.items);
+        },
+          error => {
+            if (showLoading) {
+              loading.dismiss();
+            }
+            reject(error);
+            this.presentToast(error.error.message)
+          })
+    });
+  }
+  doRefresh(event) {
+    this.loadPost(this.url, 1, false).then(() => {
+      event.target.complete()
+    }).catch(() => {
+      event.target.complete()
+    });
+  }
+
+  loadMore(event) {
+
+    this.page++;
+
+    this.loadPost(this.url, this.page, false).then(() => {
+      event.target.complete()
+    }).catch(() => {
+      event.target.complete()
+    });
+
+  }
+  async presentToast(msg) {
+
+    let toast = await this.toastCtrl.create({
+      message: msg,
+      duration: 3000,
+      position: 'bottom',
+      cssClass: 'normal-toast'
+    });
+
+    toast.present();
+
+  }
+
+  goToPostDetails(post){
+    let navigationExtras: any = {
+      queryParams: {
+        special: JSON.stringify(post)
+      }
+    };
+    this.Router.navigate(['post-details'], navigationExtras);
+  }
 }
